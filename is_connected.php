260bcd1a025c1ec6fb03bd0dc150c89af1b2dc5e
etienne-turin <etienne.turin@gmail.com>
2018-06-05 17:29:12 +0200

    ajout is_connected

---
+++ is_connected.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+
+$retour = isset($_SESSION['user_id']);
+
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
